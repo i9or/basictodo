d2aef3c5e6daefadee96ed44cce6d76a547826f8
Igor <dont@bzzr.me>
2024-01-19 23:01:22 +0000

    Add ORM

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import {Hono} from 'hono'
+import {sql} from "drizzle-orm"
+
+import {db} from "./db.ts";
+
+const query = sql`SELECT "hello world from SQLite" as text`;
+const result = db.get<{tex:string}>(query);
+console.info({result});
 
 const app = new Hono()
 app.get('/', (c) => c.text('Hello Bun!'))
