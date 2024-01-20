b4eee537e8db1ecab0f32cbc79f18a1948d42502
Igor <dont@bzzr.me>
2024-01-20 09:11:57 +0000

    Add prettier

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,16 @@
-import {Hono} from 'hono'
-import {sql} from "drizzle-orm"
+import { Hono } from "hono";
+import { sql } from "drizzle-orm";
 
-import {db} from "./db.ts";
+import { db } from "./db.ts";
 
 const query = sql`SELECT "hello world from SQLite" as text`;
-const result = db.get<{tex:string}>(query);
-console.info({result});
+const result = db.get<{ tex: string }>(query);
+console.info({ result });
 
-const app = new Hono()
-app.get('/', (c) => c.text('Hello Bun!'))
+const app = new Hono();
+app.get("/", (c) => c.text("Hello Bun!"));
 
 Bun.serve({
   fetch: app.fetch,
-  port: 1337
-})
-
+  port: 1337,
+});
