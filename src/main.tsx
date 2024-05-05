ec9bdbb9c1bd7740ee7c8072dbee70a970ce819f
Igor <dont@bzzr.me>
2024-05-05 22:47:56 +0100

    Add basic hot reload and sign in page

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { Hono } from "hono";
 import { serveStatic } from "hono/bun";
 import { sql } from "drizzle-orm";
+import { WebSocketServer } from "ws";
 
 import { db } from "./db.ts";
 import { MainPage } from "./views/MainPage.tsx";
+import { SignInPage } from "./views/SignInPage.tsx";
 
 const query = sql`SELECT "hello world from SQLite" as text`;
 const result = db.get<{ text: string }>(query);
@@ -11,9 +13,14 @@ console.info({ result });
 
 const app = new Hono();
 app.use("/public/*", serveStatic({ root: "./" }));
+app.get("/sign-in", (c) => c.html(<SignInPage />));
 app.get("/", (c) => c.html(<MainPage />));
 
-Bun.serve({
+const server = Bun.serve({
   fetch: app.fetch,
   port: 1337,
 });
+
+const wss = new WebSocketServer({ port: 1338 });
+
+console.info(`🌭 Listening on ${server.url}`);
