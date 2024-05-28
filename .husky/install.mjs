a8fbf5c1219d3d7fd17c1522d3d92554408ddd50
Igor <dont@bzzr.me>
2024-05-28 19:58:36 +0100

    Fix linting issues

--- .husky/install.mjs
+++ .husky/install.mjs
@@ -1,6 +1,9 @@
 // Skip Husky install in production and CI
-if (process.env.NODE_ENV === 'production' || process.env.CI === 'true') {
-  process.exit(0)
+const EXIT_SUCCESS = 0;
+
+if (process.env.NODE_ENV === "production" || process.env.CI === "true") {
+  process.exit(EXIT_SUCCESS);
 }
-const husky = (await import('husky')).default
-console.log(husky())
+const husky = (await import("husky")).default;
+// eslint-disable-next-line no-console
+console.log(husky());
