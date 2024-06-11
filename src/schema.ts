93f14c660ebf2deb0d759573f266742c4787d1b4
Igor <dont@bzzr.me>
2024-06-11 08:52:14 +0100

    Add basic user DB schema

--- src/schema.ts
+++ src/schema.ts
@@ -5,9 +5,25 @@ import {
   uniqueIndex,
 } from "drizzle-orm/sqlite-core";
 
+export const users = sqliteTable(
+  "users",
+  {
+    id: integer("id").primaryKey(),
+    firstName: text("first_name").notNull(),
+    lastName: text("last_name").notNull(),
+    email: text("email").notNull().unique(),
+  },
+  (self) => ({
+    emailIndex: uniqueIndex("email_index").on(self.email),
+  }),
+);
+
 export const taskLists = sqliteTable("task_lists", {
   id: integer("id").primaryKey(),
   name: text("name").notNull(),
+  userId: integer("user_id")
+    .references(() => users.id)
+    .notNull(),
 });
 
 export const tasks = sqliteTable(
@@ -21,6 +37,9 @@ export const tasks = sqliteTable(
     listId: integer("list_id")
       .references(() => taskLists.id)
       .notNull(),
+    userId: integer("user_id")
+      .references(() => users.id)
+      .notNull(),
   },
   (self) => ({
     summaryIndex: uniqueIndex("summary_index").on(self.summary),
@@ -28,4 +47,10 @@ export const tasks = sqliteTable(
 );
 
 export type TaskList = typeof taskLists.$inferSelect;
+export type TaskListDto = typeof taskLists.$inferInsert;
+
 export type Task = typeof tasks.$inferSelect;
+export type TaskDto = typeof tasks.$inferInsert;
+
+export type User = typeof users.$inferSelect;
+export type UserDto = typeof users.$inferInsert;
