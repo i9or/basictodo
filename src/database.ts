e8fe6fde71ec8e289ccc8de553f2b7120ff5fc95
Igor <dont@bzzr.me>
2024-07-02 20:43:55 +0100

    Add db file to docker configuration

--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,9 @@ import { Database } from "bun:sqlite";
 import { drizzle } from "drizzle-orm/bun-sqlite";
 
 import { ENV } from "~/env.ts";
+import { logger } from "~/utils/logger.ts";
 
+logger.info(`Database: ${ENV.dbPath}`);
 const sqlite = new Database(ENV.dbPath, { create: true });
 
 export const database = drizzle(sqlite);
